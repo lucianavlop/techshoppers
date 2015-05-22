d6fc58f182a6fa91e73383df9f06f1861b2e901a
Lucia Navaza <lucianavaza@Lucias-MacBook-Air.local>
2015-05-22 14:38:00 +0100

    Trying angular 2

--- javascripts/angular/main.ctrl.js
+++ javascripts/angular/main.ctrl.js
@@ -56,7 +56,7 @@ var control= techshop.controller("MainController", function($http, $scope, $loca
 
 
 	vm.saveInfo = function(isValid) {
-		alert ('submited: '+ $scope.userForm.submitted);	
+		alert ('submited: '+ $scope);	
 		alert ('Valid: '+ isValid);	
 			if (isValid) {
 						alert('our form is amazing');
