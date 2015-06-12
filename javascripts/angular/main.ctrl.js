a593a60fea1cbb4fd59a37708b30c08b718a0130
Lucia Navaza <lucianavaza@Lucias-MacBook-Air.local>
2015-06-12 11:00:00 +0100

    proble with autofill angular

--- javascripts/angular/main.ctrl.js
+++ javascripts/angular/main.ctrl.js
@@ -241,6 +241,25 @@ techshop.directive('match', function($parse) {
   };
 });
 
+//Directive to allow autofill and not be half filled
+techshop.directive('formAutofillFix', function() {
+  return function(scope, elem, attrs) {
+	 // Fixes Chrome bug: https://groups.google.com/forum/#!topic/angular/6NlucSskQjY
+    elem.prop('method', 'POST');
+
+    // Fix autofill issues where Angular doesn't know about autofilled inputs
+    if(attrs.ngSubmit) {
+  	  alert ("Fixing autofill");
+      setTimeout(function() {
+          elem.unbind('submit').bind('submit', function(e) {
+          e.preventDefault();
+          elem.find('input, textarea, select').trigger('input').trigger('change').trigger('keydown');
+          scope.$apply(attrs.ngSubmit);
+        });
+      }, 0);
+    }
+  };
+});
 
 
 
